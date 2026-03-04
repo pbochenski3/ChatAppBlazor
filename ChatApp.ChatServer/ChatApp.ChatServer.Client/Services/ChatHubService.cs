f80fe91f5cbb2a3e4ae59cb070dbfba35321a566
Patryk Bocheński <patrykbochenski3@gmail.com>
2026-03-04 14:43:48 +0100

    refactor: move SingalR logic to ChatHubService

---
+++ ChatApp.ChatServer/ChatApp.ChatServer.Client/Services/ChatHubService.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+public class ChatHubService : IAsyncDisposable
+{
+    private readonly string _hubUrl;
+    public HubConnection HubConnection { get; private set; }
+    public event Action<string, string>? OnMessageReceived;
+
+    public ChatHubService(IConfiguration configuration)
+    {
+        _hubUrl = configuration["SignalR:HubUrl"];
+
+        HubConnection = new HubConnectionBuilder()
+            .WithUrl(_hubUrl)
+            .WithAutomaticReconnect()
+            .Build();
+
+        HubConnection.On<string, string>("ReceiveMessage", (user, message) =>
+        {
+            OnMessageReceived?.Invoke(user, message);
+        });
+    }
+
+    public async Task StartAsync()
+    {
+        if (HubConnection.State == HubConnectionState.Disconnected)
+        {
+            await HubConnection.StartAsync();
+        }
+    }
+
+    public async ValueTask DisposeAsync()
+    {
+        if (HubConnection is not null)
+        {
+            await HubConnection.DisposeAsync();
+        }
+    }
+}
