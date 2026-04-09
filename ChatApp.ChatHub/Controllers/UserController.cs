2ac5c9e99131acedf8bc54febd8760c5e05686ca
Patryk Bocheński <patrykbochenski3@gmail.com>
2026-04-09 10:30:03 +0200

    feat: migrate userList get from SignalR to Web API

--- ChatApp.ChatHub/Controllers/UserController.cs
+++ ChatApp.ChatHub/Controllers/UserController.cs
@@ -17,14 +17,14 @@ namespace ChatApp.ChatHub.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class UserController : ControllerBase
+    public class UserController : AppControllerBase
     {
         private readonly IHubContext<ChatHub> _hubContext;
         private readonly IFileService _fileService;
         private readonly IContactService _contactService;
         private readonly IUserService _userService;
 
-        public UserController(IFileService fileService,IHubContext<ChatHub> hubContext,IContactService contactService,IUserService userService)
+        public UserController(IFileService fileService, IHubContext<ChatHub> hubContext, IContactService contactService, IUserService userService)
         {
             _hubContext = hubContext;
             _fileService = fileService;
@@ -32,24 +32,28 @@ namespace ChatApp.ChatHub.Controllers
             _userService = userService;
         }
         [Authorize]
-        [HttpPost("updateAvatar")]
+        [HttpGet]
+        public async Task<IActionResult> GetUsersToInviteAsync([FromQuery] string query)
+        {
+            var userId = CurrentUserId;
+            var usersToInvite = await _userService.GetUsersToInviteAsync(userId, query);
+            return Ok(usersToInvite);
+        }
+        [Authorize]
+        [HttpPost("Avatar")]
         public async Task<IActionResult> UploadAvatarAsync(IFormFile file)
         {
-            var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
-            if (string.IsNullOrEmpty(userId) || !Guid.TryParse(userId, out var userGuid))
-            {
-                return Unauthorized();
-            }
+            var userId = CurrentUserId;
             try
             {
                 var extension = Path.GetExtension(file.FileName).ToLower();
                 using var stream = file.OpenReadStream();
                 string avatarUrl = await _fileService.SaveAvatar(stream, extension,UploadType.UserAvatar);
-                await _userService.UpdateUserAvatarAsync(userGuid, avatarUrl);
-                var contacts = await _contactService.GetUserContactsAsync(userGuid);
+                await _userService.UpdateUserAvatarAsync(userId, avatarUrl);
+                var contacts = await _contactService.GetUserContactsAsync(userId);
                 var contactsToNofitify = contacts.Select(c => c.ContactUserID.ToString()).ToList();
-                var allRecipients = contactsToNofitify.Append(userGuid.ToString()).ToList();
-                await _hubContext.Clients.Users(allRecipients).SendAsync("ContactAvatarReload", avatarUrl, userGuid);
+                var allRecipients = contactsToNofitify.Append(userId.ToString()).ToList();
+                await _hubContext.Clients.Users(allRecipients).SendAsync("ContactAvatarReload", avatarUrl, userId);
                 return Ok();
                     }
             catch (Exception ex)
