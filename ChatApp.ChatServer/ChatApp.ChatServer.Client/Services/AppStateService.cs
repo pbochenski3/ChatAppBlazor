6c0186a3685c1432d1b2175bc9c66c47d6d2c7b4
Patryk Bocheński <patrykbochenski3@gmail.com>
2026-04-09 12:24:48 +0200

    feat: localStorage currentUser + chat

--- ChatApp.ChatServer/ChatApp.ChatServer.Client/Services/AppStateService.cs
+++ ChatApp.ChatServer/ChatApp.ChatServer.Client/Services/AppStateService.cs
@@ -2,20 +2,62 @@
 using ChatApp.Application.DTO.Chats;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.JSInterop;
+using System.Text.Json;
 
 
 namespace ChatApp.ChatServer.Client.Services
 {
     public class AppStateService
     {
+        private readonly IJSRuntime _js;
+        private const string UserKey = "current_user_session";
+        private const string ChatKey = "current_user_chat";
+        public AppStateService(IJSRuntime js)
+        {
+            _js = js;
+        }
+        public bool IsInitialized { get; private set; } = false;
         public string? Message { get; set; }
-
         public UserDTO? CurrentUser { get; set; } = null;
         public UserChatDTO? CurrentChat { get; set; } = null;
-        public void Logout()
+        public async Task LoadSessionAsync()
+        {
+            IsInitialized = false;
+            var user = await _js.InvokeAsync<string?>("localStorage.getItem", UserKey);
+            var chat = await _js.InvokeAsync<string?>("localStorage.getItem", ChatKey);
+            if(!string.IsNullOrEmpty(user))
+                {
+                CurrentUser = JsonSerializer.Deserialize<UserDTO>(user);
+                };
+            if (!string.IsNullOrEmpty(chat))
+            {
+                CurrentChat = JsonSerializer.Deserialize<UserChatDTO>(chat);
+            }
+            ;
+            IsInitialized = true;
+        }
+        public async Task SetUserAsync(UserDTO user)
+        {
+            CurrentUser = user;
+            var json = JsonSerializer.Serialize(user);
+            await _js.InvokeVoidAsync("localStorage.setItem", UserKey, json);
+        }
+        public async Task SetChatAsync(UserChatDTO chat)
+        {
+            CurrentChat = chat;
+            var json = JsonSerializer.Serialize(chat);
+            await _js.InvokeVoidAsync("localStorage.setItem", ChatKey, json);
+        }
+
+        public async Task Logout()
         {
             CurrentUser = null;
+            CurrentChat = null;
             Message = "You have been logged out.";
+            await _js.InvokeVoidAsync("localStorage.removeItem", UserKey);
+            await _js.InvokeVoidAsync("localStorage.removeItem", ChatKey);
         }
     }
 }
+
