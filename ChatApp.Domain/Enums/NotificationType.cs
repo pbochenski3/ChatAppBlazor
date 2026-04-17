f10ffb628b997ee804f60a147ed8661ae97d092f
Patryk Bocheński <patrykbochenski3@gmail.com>
2026-04-17 12:08:54 +0200

    feat: notification system (central)

---
+++ ChatApp.Domain/Enums/NotificationType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChatApp.Domain.Enums
+{
+    public enum NotificationType
+    {
+        Info,
+        Warning,
+        Error,
+        Success
+    }
+}
---
+++ ChatApp.Domain/Models/NotificationMessage.cs
@@ -0,0 +1,16 @@
+﻿using ChatApp.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChatApp.Domain.Models
+{
+    public class NotificationMessage
+    {
+        public string Id { get; } = Guid.NewGuid().ToString();
+        public string Title { get; set; } = string.Empty;
+        public string Message { get; set; } = string.Empty;
+        public NotificationType Type { get; set; }
+        public DateTime Timestamp { get; } = DateTime.UtcNow;
+    }
+}
