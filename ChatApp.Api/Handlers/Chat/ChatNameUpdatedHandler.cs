65b2cf7854790cab80922791ccb28bdc70fe6e62
Patryk Bocheński <patrykbochenski3@gmail.com>
2026-04-22 14:16:06 +0200

    refactor: changed services to repository

--- ChatApp.Api/Handlers/Chat/ChatNameUpdatedHandler.cs
+++ ChatApp.Api/Handlers/Chat/ChatNameUpdatedHandler.cs
@@ -1,12 +1,14 @@
 ﻿using Azure.Core;
 using ChatApp.Application.DTO;
 using ChatApp.Application.Interfaces.Chats;
+using ChatApp.Application.Interfaces.Repository;
 using ChatApp.Application.Interfaces.Service;
 using ChatApp.Application.Notifications.Chat;
 using ChatApp.Application.Services;
 using ChatApp.Application.Services.Chats;
 using ChatApp.Domain.Enums;
 using ChatApp.Domain.Models;
+using ChatApp.Domain.Repository;
 using MediatR;
 using Microsoft.AspNetCore.SignalR;
 
@@ -15,29 +17,28 @@ namespace ChatApp.Api.Handlers.Chat
     public class ChatNameUpdatedHandler : INotificationHandler<ChatNameUpdatedNotification>
     {
         private readonly IHubContext<ChatHub> _hubContext;
-        private readonly IChatService _chatService;
-        private readonly IMessageService _messageService;
-        public ChatNameUpdatedHandler(IMessageService messageService, IChatService chatService,IHubContext<ChatHub> hubContext)
+        private readonly IUserChatRepository _userChatRepo;
+        private readonly IMessageRepository _messageRepo;
+        public ChatNameUpdatedHandler(IMessageRepository messageRepo, IUserChatRepository userChatRepo,IHubContext<ChatHub> hubContext)
         {
-            _messageService = messageService;
-            _chatService = chatService;
+            _messageRepo = messageRepo;
+            _userChatRepo = userChatRepo;
             _hubContext = hubContext;
 
         }
         public async Task Handle(ChatNameUpdatedNotification n, CancellationToken cancellationToken)
         {
-            var usersInChat = await _chatService.GetUsersInChatIdAsync(n.ChatId);
+            var usersInChat = await _userChatRepo.GetUsersInChatIdAsync(n.ChatId);
             var usersToNofitify = usersInChat.Select(u => u.ToString()).ToList();
-            var systemMessage = new MessageDTO
+            var systemMessage = new Domain.Models.Message
             {
                 ChatID = n.ChatId,
                 MessageID = Guid.CreateVersion7(),
                 Content = $"{n.Request.AdminName} zmienił nazwe czatu na {n.Request.NewName}.",
-                SenderUsername = "SYSTEM",
                 MessageType = MessageType.System,
                 SentAt = DateTime.UtcNow,
             };
-            await _messageService.SaveMessageAsync(systemMessage);
+            await _messageRepo.AddMessageAsync(systemMessage);
             await _hubContext.Clients.Users(usersToNofitify).SendAsync("ReceiveMessage", systemMessage);
             await _hubContext.Clients.Users(usersToNofitify).SendAsync("UpdateChatName", n.ChatId, n.Request.NewName);
         }
